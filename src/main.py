22d82f12b077740e97db5783afb981fd19f5e045
rmn@mbp <rohoehn123@gmail.com>
2022-04-16 09:30:16 +0200

    refactored and renamed

--- src/main.py
+++ src/main.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 
-from gymLogger import GymLogger
+from printer import PrintingManager
+from database import DatabaseManager
+from utility import UtilityManager
+
+
+
+class Main:
+    def __init__(self, userprofile: str, DEBUGMODE=False) -> None:
+
+        self.utilityManager = UtilityManager()
+        self.printingManager = PrintingManager()
+        self.databaseManager = DatabaseManager(userprofile)
+
+        if DEBUGMODE:
+
+            self.printingManager.workouts(self.utilityManager.parseWorkouts())
+            print("EXISTS?", self.databaseManager.databaseExists())
+
+
+    def run(self):
+        pass
+
+
+
+
+
 
 if __name__ == "__main__":
-    
-    program = GymLogger(userprofile="roman", DEBUGMODE=True)
-    program.run()
+    Main(userprofile="roman", DEBUGMODE=True).run()
